1f356a689b6fb30e306b55bbf85564152f5a5b24
FarrasF <farrasfairuz08@gmail.com>
2026-04-09 01:34:55 +0700

    Create SetHimpunan.py

---
+++ Materi kuliah/Struktur data/Pertemuan 4/SetHimpunan.py
@@ -0,0 +1,11 @@
+buah = {"durian", "salak", "pisang"}
+
+print("Buah:", buah)
+
+# menambah buah
+buah.add("sirsak")
+print("buat setelah ditambah:", buah)
+
+# menghapus buah
+buah.remove("salak")
+print("Setelah dikurangi:", buah)
